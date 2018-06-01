c38cd7c5ce3207ce57a3efd45fd6f65c955f43e7
mittalakshay6 <mittalakshay6@gmail.com>
2018-06-01 11:25:19 +0530

    Inital Commit

---
+++ twitterscraper/__init__.py
@@ -0,0 +1,14 @@
+w# TwitterScraper
+# Copyright 2016-2018 Ahmet Taspinar
+# See LICENSE for details.
+"""
+Twitter crawler and information refiner
+"""
+
+__version__ = '1.0'
+__author__ = 'Akshay Mittal'
+__license__ = 'Pyenoma'
+
+
+from twitterscraper.query import query_tweets
+from twitterscraper.tweet import Tweet
