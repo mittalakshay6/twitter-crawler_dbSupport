06a09deb9583105b2f96836fdfaa2f826b9eb437
mittalakshay6 <mittalakshay6@gmail.com>
2018-06-06 14:22:18 +0530

    updated retrieve functions(tweet_html, tweet_url)

--- setup.py
+++ setup.py
@@ -7,17 +7,16 @@ with open('requirements.txt') as requirements:
     required = requirements.read().splitlines()
 
 setup(
-    name='twitterscraper',
-    version='0.7.0',
-    description='Tool for scraping Tweets',
-    url='https://github.com/taspinar/twitterscraper',
-    author=['Ahmet Taspinar', 'Lasse Schuirmann'],
-    author_email='taspinar@gmail.com',
-    license='MIT',
+    name='crawltwitter',
+    version='1.0',
+    description='Tool for crawling twitter for tweets regarding particular interest',
+    author=['Akshay Mittal'],
+    author_email='it158040@mnnit.ac.in',
+    license='Pyenoma',
     packages=find_packages(exclude=["build.*", "tests", "tests.*"]),
     install_requires=required,
     entry_points={
         "console_scripts": [
-            "twitterscraper = twitterscraper.main:main"
+            "crawltwitter = crawltwitter.main:main"
         ]
     })
